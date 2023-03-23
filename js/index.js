5c411cbe801362d13378246f55a9b864fb2f81a7
Ahmadjonbeginner <ahmadjonu333@gmail.com>
2023-03-23 17:30:57 +0500

    4-oy 1-Dars uyga vazifa

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // 4-oy Uyga Vazifa va Dars vaqtidagi amaliyotlar
+
+// 4.1.1-Dars Uyga Vazifa
+// const array = [1, 2, 3, 4, 5, 6]
+// const findFun = function(array, element) {
+//     const testArr = array.indexOf(element)
+//     if (testArr === -1) {
+//         return console.log([]);
+//     } else {
+//         return console.log([testArr]);
+//     }
+// }
+
+
+// 4.1 .2
+// let people = [
+//     { name: "Alice", age: 25 },
+//     { name: "Bob", age: 30 },
+//     { name: "Charlie", age: 35 }
+// ];
+
+// const myFun = function(allPeople, userAge) {
+//     const checkPeople = allPeople.every((set) => set.age >= userAge)
+
+//     if (checkPeople) {
+//         return 'katta';
+//     } else {
+//         return 'kachik';
+//     }
+// }
